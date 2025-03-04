a2940cb6bec1ee7be1492b5ddc33efd82959f167
pavan C A <pavan.a1@s.amity.edu>
2025-03-04 11:23:05 +0530

    second commit

--- pavan5.c
+++ pavan5.c
@@ -4,4 +4,14 @@ void main()
 	int n;
 	printf("enter the number");
 	scanf("%d",n);
+        if(n%2==0)
+	{
+		printf("the given number is even");
+	}
+	else
+	{
+		printf("the given number is odd");
+	}
+	return 0;
+}
 
