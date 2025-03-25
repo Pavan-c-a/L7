bb385caac1af470d67974c6b6aae3023a77262d0
pavan C A <pavan.a1@s.amity.edu>
2025-03-25 12:03:06 +0530

    case1 of c

---
+++ p.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+        int a;
+        printf("enter your choice");
+        scanf("%d",&a);
+        switch(a)
+        {
+                case 1:printf("Btech in CSE");
+                       break;
